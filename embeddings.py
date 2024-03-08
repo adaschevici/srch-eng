31cec8b36ed033108dba5af7606fe4614f386bb8
Artur Das <a.daschevici@gmail.com>
2024-03-08 23:08:30 +0100

    Fix embedding

--- embeddings.py
+++ embeddings.py
@@ -9,7 +9,7 @@ class LocalHuggingFaceEmbeddings(Embeddings):
     def __init__(self, model_name: str):
         self.model = SentenceTransformer(model_name)
 
-    def embed(self, sentences: List[str]) -> List[Tensor] | Tensor | ndarray:
+    def embed_documents(self, sentences: List[str]) -> List[Tensor] | Tensor | ndarray:
         return self.model.encode(sentences)
 
     def embed_query(self, text: str) -> List[float]:
